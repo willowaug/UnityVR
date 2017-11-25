84dc0911588ea6b71b92b911274998316af9773d
sss <a0982836293@gmail.com>
2017-11-25 14:53:34 +0800

    1125修感

--- Assets/Scripts/ShootingGalleryController.cs
+++ Assets/Scripts/ShootingGalleryController.cs
@@ -3,8 +3,105 @@ using System.Collections.Generic;
 using UnityEngine;
 using VRStandardAssets.Common;
 using VRStandardAssets.Utils;
-using UnityEngine.UI;
-public class ShootingGalleryController : MonoBehaviour
+using UnityEngine.UI;//nameSpace 命名空間
+public class ShootingGalleryController : MonoBehaviour//class 類別
 {
+    public UIController uiController;//fileld 欄位
+    public Reticle reticle;
+    public SelectionRadial selectionRadial;
+    public SelectionSlider selectionSlider;
+
+    public Image timerBar;
+    public float gameDuration = 30f;
+    public float endDelay = 1.5f;
+
+    public Collider spawnCollider;
+    public ObjectPool targetObjectPool;//標靶
+    public float spawnProbabilty = 0.7f;
+    public float spawnInterval = 1f;
+
+    public bool IsPlaying//屬性
+    {
+        private set;
+        get;
+    }
+
+
+    private IEnumerator Start()//方法
+    {
+        SessionData.SetGameType(SessionData.GameType.SHOOTER180);
+        while (true)
+        {
+            Debug.Log("Start StartPhase");
+            yield return StartCoroutine(StartPhase());
+            Debug.Log("Start PlayPhase");
+            yield return StartCoroutine(PlayPhase());
+            Debug.Log("Start EndPhase");
+            yield return StartCoroutine(EndPhase());
+            Debug.Log("Complete");
+        }
+    }
+
+    private IEnumerator StartPhase()
+    {
+        yield return StartCoroutine(uiController.ShowIntroUI());
+        reticle.Show();
+        selectionRadial.Hide();
+        yield return StartCoroutine(selectionSlider.WaitForBarToFill());
+        yield return StartCoroutine(uiController.HideIntroUI());
+
+    }
+
+    private IEnumerator PlayPhase()
+    {
+        yield return StartCoroutine(uiController.ShowPlayerUI());
+        IsPlaying = true;
+        reticle.Show();
+        SessionData.Restart();
+        float gameTimer = gameDuration;
+        float spawnTimer = 0f;
+        while (gameTimer > 0f)
+        {
+            if(spawnTimer <= 0f)
+            {
+                if(Random.value < spawnProbabilty)
+                {
+                    spawnTimer = spawnInterval;
+                    Spawn();
+                }
+            }
+            yield return null;
+            gameTimer -= Time.deltaTime;
+            spawnTimer -= Time.deltaTime;
+            timerBar.fillAmount = gameTimer / gameDuration;
+        }
+        IsPlaying = false;
+        yield return StartCoroutine(uiController.HidePlayerUI());
+    }
+    private void Spawn()
+    {
+        GameObject target = targetObjectPool.GetGameObjectFromPool();
+        target.transform.position = SpawnPosition();
+    }
+
+    private Vector3 SpawnPosition() //方法宣告 不會回傳值 >void  會回傳值>Vector3
+    {
+        Vector3 center = spawnCollider.bounds.center;//碰撞體中心點
+        Vector3 extents = spawnCollider.bounds.extents;
+        float x = Random.Range(center.x - extents.x, center.x + extents.x);
+        float y = Random.Range(center.y - extents.y, center.y + extents.y);
+        float z = Random.Range(center.z - extents.z, center.z + extents.z);
+        return new Vector3(x, y, z);
+    }
+    private IEnumerator EndPhase()
+    {
+        reticle.Hide();
+        yield return StartCoroutine(uiController.ShowOutroUI());
+        yield return new WaitForSeconds(endDelay);
+        yield return StartCoroutine(selectionRadial.WaitForSelectionRadialToFill());
+        yield return StartCoroutine(uiController.HideOutroUI());
+    }
+
+   
 
 }
--- Assets/Scripts/ShootingTarget.cs
+++ Assets/Scripts/ShootingTarget.cs
@@ -7,5 +7,91 @@ using VRStandardAssets.Common;
 
 public class ShootingTarget : MonoBehaviour
 {
+    public int score = 1;
+    public float destroyTimeOutDuration = 2f;
+    public float timeOutDuration = 2f;
+    public event Action<ShootingTarget> OnRemove;
+    private Transform cameraTransform;
+    public AudioSource audioSource;
+    private VRInteractiveItem vrInteractiveItem;
+    private Renderer mRenderer;
+    private Collider mCollider;
+    public AudioClip destroyClip;
+    public GameObject destroyPrefab;
+    public AudioClip spawnClip;
+    public AudioClip missedClip;
 
+    private bool isEnding;
+
+    private void Awake()
+    {
+        cameraTransform = Camera.main.transform;
+        audioSource = GetComponent<AudioSource>();
+        vrInteractiveItem = GetComponent<VRInteractiveItem>();
+        mRenderer = GetComponent<Renderer>();
+        mCollider = GetComponent<Collider>();
+    }
+
+    private void OnEnable()
+    {
+        vrInteractiveItem.OnDown += HandleDown;
+    }
+
+    private void OnDisable()
+    {
+        vrInteractiveItem.OnDown -= HandleDown;
+    }
+
+    private void OnDestroy()
+    {
+        OnRemove = null;
+    }
+    private void HandleDown()
+    {
+        StartCoroutine(OnHit());
+    }
+
+    private IEnumerator OnHit()
+    {
+        if (isEnding)
+            yield break;
+        isEnding = true;
+        mRenderer.enabled = false;
+        mCollider.enabled = false;
+        audioSource.clip = destroyClip;
+        audioSource.Play();
+        SessionData.AddScore(score);
+        GameObject destroyedTarget = Instantiate<GameObject>(destroyPrefab, transform.position, transform.rotation);
+        Destroy(destroyedTarget, destroyTimeOutDuration);//幾秒後會死掉
+        yield return new WaitForSeconds(destroyClip.length);
+        if (OnRemove != null)
+            OnRemove(this);
+    }
+
+    public void Restart()
+    {
+        mRenderer.enabled = true;
+        mCollider.enabled = true;
+        isEnding = false;
+        audioSource.clip = spawnClip;
+        audioSource.Play();
+        transform.LookAt(cameraTransform.position);
+        StartCoroutine(MissTarget());
+
+    }
+
+    private IEnumerator MissTarget()
+    {
+        yield return new WaitForSeconds(timeOutDuration);
+        if (isEnding)
+            yield break;
+        isEnding = true;
+        mRenderer.enabled = false;
+        mCollider.enabled = false;
+        audioSource.clip = missedClip;
+        yield return new WaitForSeconds(missedClip.length);
+        if (OnRemove != null)
+            OnRemove(this);
+    }
 }
+
